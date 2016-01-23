aff89133f40ed4a4dd54bea2486228863c379a6e
Alvin James Bellero <ajames.bellero@gmail.com>
2016-01-23 18:05:23 +0800

    Added validation for missing contact name

--- api/controllers/ContactsController.js
+++ api/controllers/ContactsController.js
@@ -1,5 +1,7 @@
 module.exports = {
   save: function (req, res) {
+  	if(!req.body.contact_name)
+  		return res.send("Missing contact name");
     return res.send("Contact saved!");
   }
 };
--- test/unit/controllers/ContactsController.test.js
+++ test/unit/controllers/ContactsController.test.js
@@ -3,16 +3,32 @@ var request = require('supertest');
 describe('ContactsController', function() {
 
   describe('#save()', function() {
+  	var STATUS_SUCCESS = 200;
+
   	describe('with valid params', function() {
-	    it('should redirect to /contact ', function (done) {
-	   	  var validParam = { name: 'test', password: 'test' };
+	    it('should redirect to /contact with success message', function (done) {
+	   	  var validParam = { contact_name: 'test', contact_num: 'test' };
 
 	      request(sails.hooks.http.app)
 	        .post('/contacts')
 	        .send(validParam)
 	        .expect('Content-Type', /text/)
-			.expect(200)
+			.expect(STATUS_SUCCESS)
 			.expect('Contact saved!', done);
+	    });
+    });
+
+    describe('with missing params', function() {
+	    it('should redirect to /contact with Missing contact name', function (done) {
+	   	  var missingContactName = { contact_name: '', contact_num: '12345' },
+	   	  	expectedResult = 'Missing contact name';
+
+	      request(sails.hooks.http.app)
+	        .post('/contacts')
+	        .send(missingContactName)
+	        .expect('Content-Type', /text/)
+			.expect(STATUS_SUCCESS)
+			.expect(expectedResult, done);
 	    });
     });
   });
