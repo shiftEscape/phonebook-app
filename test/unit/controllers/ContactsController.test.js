629dc1062204e273cefbe5e1d22c65a6a860e383
Alvin James Bellero <ajames.bellero@gmail.com>
2016-01-23 18:37:20 +0800

    Refactor code

--- test/unit/controllers/ContactsController.test.js
+++ test/unit/controllers/ContactsController.test.js
@@ -2,57 +2,43 @@ var request = require('supertest');
 
 describe('ContactsController', function() {
 
+  var STATUS_SUCCESS = 200;
+
   describe('#save()', function() {
-  	var STATUS_SUCCESS = 200;
+  	
+  	function execRequest(param, expectedResult, done) {
+  		request(sails.hooks.http.app)
+	        .post('/contacts')
+	        .send(param)
+	        .expect('Content-Type', /text/)
+			.expect(STATUS_SUCCESS)
+			.expect(expectedResult, done);
+  	}
 
   	describe('with valid params', function() {
 	    it('should redirect to /contact with success message', function (done) {
-	   	  var validParam = { contact_name: 'test', contact_num: 'test' };
-
-	      request(sails.hooks.http.app)
-	        .post('/contacts')
-	        .send(validParam)
-	        .expect('Content-Type', /text/)
-			.expect(STATUS_SUCCESS)
-			.expect('Contact saved!', done);
-	    });
+	   	  var validParam = { contact_name: 'test', contact_num: '123' };
+	   	  execRequest(validParam, 'Contact saved!', done);
+	   	});
     });
 
     describe('with missing params', function() {
 	    it('should redirect to /contact with Missing contact name', function (done) {
 	   	  var missingContactName = { contact_name: '', contact_num: '12345' },
 	   	  	expectedResult = 'Missing contact name';
-
-	      request(sails.hooks.http.app)
-	        .post('/contacts')
-	        .send(missingContactName)
-	        .expect('Content-Type', /text/)
-			.expect(STATUS_SUCCESS)
-			.expect(expectedResult, done);
+	   	  execRequest(missingContactName, expectedResult, done);
 	    });
 
 	    it('should redirect to /contact with Missing contact number', function (done) {
 	   	  var missingContactNumber = { contact_name: 'Joe Joey', contact_num: '' },
 	   	  	expectedResult = 'Missing contact number';
-
-	      request(sails.hooks.http.app)
-	        .post('/contacts')
-	        .send(missingContactNumber)
-	        .expect('Content-Type', /text/)
-			.expect(STATUS_SUCCESS)
-			.expect(expectedResult, done);
+	      execRequest(missingContactNumber, expectedResult, done);
 	    });
 
 	    it('should redirect to /contact with Missing contact name and number', function (done) {
 	   	  var missingContact = { contact_name: '', contact_num: '' },
 	   	  	expectedResult = 'Missing contact details';
-
-	      request(sails.hooks.http.app)
-	        .post('/contacts')
-	        .send(missingContact)
-	        .expect('Content-Type', /text/)
-			.expect(STATUS_SUCCESS)
-			.expect(expectedResult, done);
+		  execRequest(missingContact, expectedResult, done);
 	    });
     });
   });
