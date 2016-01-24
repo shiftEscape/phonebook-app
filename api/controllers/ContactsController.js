e07f343e3c9e2ca876ba53a298c7ded84f5d7a57
Alvin James Bellero <ajames.bellero@gmail.com>
2016-01-24 12:42:25 +0800

    Modified code indentations

--- api/controllers/ContactsController.js
+++ api/controllers/ContactsController.js
@@ -1,18 +1,18 @@
 function validate(param) {
-	var result = 'Contact saved!';
+  var result = 'Contact saved!';
 
-	if(!param.contact_name && !param.contact_num)
-  		result = "Missing contact details";
-  	else if(!param.contact_name)
-  		result = "Missing contact name";
-  	else if(!param.contact_num)
-  		result = "Missing contact number";
+  if(!param.contact_name && !param.contact_num)
+      result = "Missing contact details";
+  else if(!param.contact_name)
+    result = "Missing contact name";
+  else if(!param.contact_num)
+    result = "Missing contact number";
 
-  	return result;
+  return result;
 }
 
 module.exports = {
   save: function (req, res) {
-  	return res.send(validate(req.body));
+    return res.send(validate(req.body));
   }
 };
--- test/unit/controllers/ContactsController.test.js
+++ test/unit/controllers/ContactsController.test.js
@@ -5,42 +5,43 @@ describe('ContactsController', function() {
   var STATUS_SUCCESS = 200;
 
   describe('#save()', function() {
-  	
-  	function execRequest(param, expectedResult, done) {
-  		request(sails.hooks.http.app)
-	        .post('/contacts')
-	        .send(param)
-	        .expect('Content-Type', /text/)
-			.expect(STATUS_SUCCESS)
-			.expect(expectedResult, done);
-  	}
 
-  	describe('with valid params', function() {
-	    it('should redirect to /contact with success message', function (done) {
-	   	  var validParam = { contact_name: 'test', contact_num: '123' };
-	   	  execRequest(validParam, 'Contact saved!', done);
-	   	});
+    function execRequest(param, expectedResult, done) {
+      request(sails.hooks.http.app)
+          .post('/contacts')
+          .send(param)
+          .expect('Content-Type', /text/)
+      .expect(STATUS_SUCCESS)
+      .expect(expectedResult, done);
+    }
+
+    describe('with valid params', function() {
+      it('should redirect to /contact with success message', function (done) {
+        var validParam = { contact_name: 'test', contact_num: '123' };
+        execRequest(validParam, 'Contact saved!', done);
+      });
     });
 
     describe('with missing params', function() {
-	    it('should redirect to /contact with Missing contact name', function (done) {
-	   	  var missingContactName = { contact_name: '', contact_num: '12345' },
-	   	  	expectedResult = 'Missing contact name';
-	   	  execRequest(missingContactName, expectedResult, done);
-	    });
+      it('should redirect to /contact with Missing contact name', function (done) {
+        var missingContactName = { contact_name: '', contact_num: '12345' },
+          expectedResult = 'Missing contact name';
+        execRequest(missingContactName, expectedResult, done);
+      });
 
-	    it('should redirect to /contact with Missing contact number', function (done) {
-	   	  var missingContactNumber = { contact_name: 'Joe Joey', contact_num: '' },
-	   	  	expectedResult = 'Missing contact number';
-	      execRequest(missingContactNumber, expectedResult, done);
-	    });
+      it('should redirect to /contact with Missing contact number', function (done) {
+        var missingContactNumber = { contact_name: 'Joe Joey', contact_num: '' },
+          expectedResult = 'Missing contact number';
+        execRequest(missingContactNumber, expectedResult, done);
+      });
 
-	    it('should redirect to /contact with Missing contact name and number', function (done) {
-	   	  var missingContact = { contact_name: '', contact_num: '' },
-	   	  	expectedResult = 'Missing contact details';
-		  execRequest(missingContact, expectedResult, done);
-	    });
+      it('should redirect to /contact with Missing contact name and number', function (done) {
+        var missingContact = { contact_name: '', contact_num: '' },
+          expectedResult = 'Missing contact details';
+        execRequest(missingContact, expectedResult, done);
+      });
     });
+
   });
 
 });
