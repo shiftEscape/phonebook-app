be20b3974f33d4665e9d6a42c2e23c32880008cc
Alvin James Bellero <ajames.bellero@gmail.com>
2016-01-25 02:16:48 +0800

    Refactored ContactController unit test

--- test/unit/controllers/ContactController.test.js
+++ test/unit/controllers/ContactController.test.js
@@ -16,7 +16,7 @@ describe('ContactsController', function() {
     }
 
     describe('with valid params', function() {
-      it('should redirect to /contact with success message after contact creation', function (done) {
+      it('should return a success message after contact creation', function (done) {
         var validParam = { contact_name: 'test', contact_num: '123' },
           expectedResult = 'Contact saved!';
         execRequest(validParam, expectedResult, done);
@@ -24,19 +24,19 @@ describe('ContactsController', function() {
     });
 
     describe('with missing params', function() {
-      it('should redirect to /contact with missing contact name', function (done) {
+      it('should return an error if contact name is missing', function (done) {
         var missingContactName = { contact_name: '', contact_num: '12345' },
           expectedResult = 'Missing contact name';
         execRequest(missingContactName, expectedResult, done);
       });
 
-      it('should redirect to /contact with missing contact number', function (done) {
+      it('should return an error if contact number is missing', function (done) {
         var missingContactNumber = { contact_name: 'Joe Joey', contact_num: '' },
           expectedResult = 'Missing contact number';
         execRequest(missingContactNumber, expectedResult, done);
       });
 
-      it('should redirect to /contact with missing contact name and number', function (done) {
+      it('should return an error if contact name and number are missing', function (done) {
         var missingContact = { contact_name: '', contact_num: '' },
           expectedResult = 'Missing contact details';
         execRequest(missingContact, expectedResult, done);
@@ -57,7 +57,7 @@ describe('ContactsController', function() {
     }
 
     describe('with valid params', function() {
-      it('should redirect to /contact with success message after updating', function (done) {
+      it('should return a success message after contact update', function (done) {
         var validParam = { contact_name: 'test', contact_num: '123' },
           expectedResult = 'Contact updated!';
         execRequest(validParam, expectedResult, done);
@@ -65,19 +65,19 @@ describe('ContactsController', function() {
     });
 
     describe('with missing params', function() {
-      it('should redirect to /contact with missing contact name', function (done) {
+      it('should return an error if contact name is missing', function (done) {
         var missingContactName = { contact_name: '', contact_num: '12345' },
           expectedResult = 'Missing contact name';
         execRequest(missingContactName, expectedResult, done);
       });
 
-      it('should redirect to /contact with missing contact number', function (done) {
+      it('should return an error if contact number is missing', function (done) {
         var missingContactNumber = { contact_name: 'Joe Joey', contact_num: '' },
           expectedResult = 'Missing contact number';
         execRequest(missingContactNumber, expectedResult, done);
       });
 
-      it('should redirect to /contact with missing contact name and number', function (done) {
+      it('should return an error if contact name and number are missing', function (done) {
         var missingContact = { contact_name: '', contact_num: '' },
           expectedResult = 'Missing contact details';
         execRequest(missingContact, expectedResult, done);
@@ -97,12 +97,12 @@ describe('ContactsController', function() {
     }
 
     describe('with valid param', function() {
-      it('should return all contacts stored in DB', function (done) {
+      it('should return a success message if ALL contact has been retrieved', function (done) {
         expectedResult = 'All contacts retrieved!';
         execRequest('/contact', expectedResult, done);
       });
 
-      it('should return one contact based on ID passed', function (done) {
+      it('should return a success message if ONE contact has been retrieved', function (done) {
         expectedResult = 'One contact retrieved!';
         execRequest('/contact/1', expectedResult, done);
       });
@@ -121,7 +121,7 @@ describe('ContactsController', function() {
     }
 
     describe('with valid param', function() {
-      it('should redirect to /contact with success message after deleting', function (done) {
+      it('should return a success message after contact deletion', function (done) {
         expectedResult = 'Contact deleted!';
         execRequest(expectedResult, done);
       });
