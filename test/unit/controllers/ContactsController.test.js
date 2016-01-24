6f1d5e552fcdab39855ca0de42a01946ecb3d252
Alvin James Bellero <ajames.bellero@gmail.com>
2016-01-24 13:53:03 +0800

    Added unit test for #update() controller

--- test/unit/controllers/ContactsController.test.js
+++ test/unit/controllers/ContactsController.test.js
@@ -2,40 +2,82 @@ var request = require('supertest');
 
 describe('ContactsController', function() {
 
-  var STATUS_SUCCESS = 200;
+  const STATUS_SUCCESS = 200;
 
   describe('#save()', function() {
 
     function execRequest(param, expectedResult, done) {
       request(sails.hooks.http.app)
-          .post('/contacts')
-          .send(param)
-          .expect('Content-Type', /text/)
+      .post('/contacts')
+      .send(param)
+      .expect('Content-Type', /text/)
       .expect(STATUS_SUCCESS)
       .expect(expectedResult, done);
     }
 
     describe('with valid params', function() {
-      it('should redirect to /contact with success message', function (done) {
-        var validParam = { contact_name: 'test', contact_num: '123' };
-        execRequest(validParam, 'Contact saved!', done);
+      it('should redirect to /contact with success message after contact creation', function (done) {
+        var validParam = { contact_name: 'test', contact_num: '123' },
+          expectedResult = 'Contact saved!';
+        execRequest(validParam, expectedResult, done);
       });
     });
 
     describe('with missing params', function() {
-      it('should redirect to /contact with Missing contact name', function (done) {
+      it('should redirect to /contact with missing contact name', function (done) {
         var missingContactName = { contact_name: '', contact_num: '12345' },
           expectedResult = 'Missing contact name';
         execRequest(missingContactName, expectedResult, done);
       });
 
-      it('should redirect to /contact with Missing contact number', function (done) {
+      it('should redirect to /contact with missing contact number', function (done) {
         var missingContactNumber = { contact_name: 'Joe Joey', contact_num: '' },
           expectedResult = 'Missing contact number';
         execRequest(missingContactNumber, expectedResult, done);
       });
 
-      it('should redirect to /contact with Missing contact name and number', function (done) {
+      it('should redirect to /contact with missing contact name and number', function (done) {
+        var missingContact = { contact_name: '', contact_num: '' },
+          expectedResult = 'Missing contact details';
+        execRequest(missingContact, expectedResult, done);
+      });
+    });
+
+  });
+
+  describe('#update()', function() {
+
+    function execRequest(param, expectedResult, done) {
+      request(sails.hooks.http.app)
+        .put('/contacts/1')
+        .send(param)
+        .expect('Content-Type', /text/)
+        .expect(STATUS_SUCCESS)
+        .expect(expectedResult, done);
+    }
+
+    describe('with valid params', function() {
+      it('should redirect to /contact with success message after updating', function (done) {
+        var validParam = { contact_name: 'test', contact_num: '123' },
+          expectedResult = 'Contact updated!';
+        execRequest(validParam, expectedResult, done);
+      });
+    });
+
+    describe('with missing params', function() {
+      it('should redirect to /contact with missing contact name', function (done) {
+        var missingContactName = { contact_name: '', contact_num: '12345' },
+          expectedResult = 'Missing contact name';
+        execRequest(missingContactName, expectedResult, done);
+      });
+
+      it('should redirect to /contact with missing contact number', function (done) {
+        var missingContactNumber = { contact_name: 'Joe Joey', contact_num: '' },
+          expectedResult = 'Missing contact number';
+        execRequest(missingContactNumber, expectedResult, done);
+      });
+
+      it('should redirect to /contact with missing contact name and number', function (done) {
         var missingContact = { contact_name: '', contact_num: '' },
           expectedResult = 'Missing contact details';
         execRequest(missingContact, expectedResult, done);
